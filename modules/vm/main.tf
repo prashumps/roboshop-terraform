d2fd22f5a14660259c3e7e67985515d7acb1013f
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-07-04 10:16:11 +0530

    learn devops

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_virtual_machine" "vm" {
 #   provisioner "remote-exec" {
 #     inline = [
 #       "sudo dnf install python3.12 python3.12-pip -y",
-#       "sudo pip3.12 install ansible",
+#       "sudo pip3.12 install ansible hvac",
 #       "ansible-pull -i localhost, -U https://github.com/prashump/roboshop-ansible roboshop.yml -e app_name=${var.name}"
 #     ]
 #   }
