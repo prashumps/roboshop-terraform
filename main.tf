9519b1d92d328dac8f3703341bac9020f95e361e
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-07-12 09:37:22 +0530

    learn devops

--- main.tf
+++ main.tf
@@ -42,4 +42,6 @@ module "aks" {
   name = each.key
   rg_name                       = module.resource-group[each.value["rgname"]].name
   rg_location                   = module.resource-group[each.value["rgname"]].location
+  env                           = var.env
+  token                         = var.token
 }
