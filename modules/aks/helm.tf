50196b50836d38dbd24c4c4fddf576216cda6dfd
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-08-23 17:13:08 +0530

    learn devops

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -8,10 +8,17 @@ terraform {
 }
 
 resource "null_resource" "kubeconfig" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [
+    azurerm_kubernetes_cluster.main
+  ]
+
   provisioner "local-exec" {
     command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing"
   }
-
 }
 
 resource "helm_release" "external-secrets" {
