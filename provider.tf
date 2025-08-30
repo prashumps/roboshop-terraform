cc93b1ba1cb0acce2f567986ec9628a3f6487f8e
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-08-30 23:54:59 +0530

    learn devops

--- provider.tf
+++ provider.tf
@@ -11,9 +11,3 @@ provider "vault" {
   address = "http://vault.prashumps.online:8200"
   token = var.token
 }
-
-# provider "helm" {
-#   kubernetes = {
-#     config_path = "~/.kube/config"
-#   }
-# }
